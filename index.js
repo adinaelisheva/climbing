0e0574109a011ed30bd87a3ccc431eaf12ff37bd
Adina Karantza <adina.elisheva@gmail.com>
2025-01-03 22:42:47 +0000

    listing climbs

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
-let logDiv, numSpan, perRouteSection, colorInput, difficultyInput, pctInput, notesInput;
+let climbsDiv, logDiv, numSpan, dateInput, perRouteSection, colorInput, difficultyInput, pctInput, companionsInput, notesInput;
 
 function setup() {
+  fetchAndUpdateRouteInfo();
+  fetchAndUpdateClimbs();
+
+  climbsDiv = document.querySelector('.climbs');
+
+  // Set up logging
   logDiv = document.querySelector('.log');
   numSpan = logDiv.querySelector('.lognum');
+  dateInput = logDiv.querySelector('input[type="date"]');
   perRouteSection = logDiv.querySelector('.perRouteValues');
   colorInput = logDiv.querySelector('input.color');
   difficultyInput = logDiv.querySelector('input.difficulty');
   pctInput = logDiv.querySelector('input.pct');
+  companionsInput = logDiv.querySelector('input.companions');
   notesInput = logDiv.querySelector('input.notes');
-  logDiv.querySelector('input[type="date"]').value = getTodayDateString();
+  dateInput.value = getTodayDateString();
+}
+
+function openClimbs() {
+  climbsDiv.classList.remove('hidden');
+}
+
+function closeClimbs() {
+  climbsDiv.classList.add('hidden');
 }
 
 function openLog(routeNum) {
@@ -34,21 +50,102 @@ function openLog(routeNum) {
 function closeLog() {
   const routeNum = numSpan.innerText;
   const routeDiv = document.querySelector('#id' + routeNum);
-  if (colorInput.value) {
+  const newColor = colorInput.value;
+  const newDiff = difficultyInput.value;
+  if (newColor) {
     const curColor = getColorFromClasslist(routeDiv);
-    if (curColor && curColor !== colorInput.value) {
+    if (curColor && curColor !== newColor) {
       colorInput.classList.remove(curColor);
     }
-    routeDiv.classList.add(colorInput.value);
+    routeDiv.classList.add(newColor);
   }
-  if (difficultyInput.value) {
-    routeDiv.setAttribute('difficulty', difficultyInput.value);
+  if (newDiff) {
+    routeDiv.setAttribute('difficulty', newDiff);
+  }
+  if (newColor !== logDiv.getAttribute('originalColor') ||
+      newDiff !== logDiv.getAttribute('originalDifficulty')) {
+    updateRoute(routeNum, newColor, newDiff);
   }
   logDiv.classList.add('hidden');
   const inputsToReset = perRouteSection.querySelectorAll('input');
   for (const i of inputsToReset) {
     i.value = '';
   }
+}
+
+async function fetchAndUpdateClimbs() {
+  const climbs = await fetchEndpoint('fetchclimbs');
+  const dialogDiv = climbsDiv.querySelector('.dialog');
+  const climbsByDate = {};
+  for (c of climbs) {
+    dateIndex = Number(c.date.replaceAll('-',''));
+    if (!climbsByDate[dateIndex]) {
+      climbsByDate[dateIndex] = {date: c.date, companions: c.companions, climbs: []};
+    }
+    climbsByDate[dateIndex].climbs.push({routenum: c.routenum, pct: c.pct, notes: c.notes});
+  }
+  for(let k of Object.keys(climbsByDate).sort()) {
+    const climb = climbsByDate[k];
+    const h = document.createElement('h3');
+    h.innerText = `${climb.date} (${climb.companions})`
+    dialogDiv.appendChild(h);
+    for (let c of climb.climbs) {
+      const d = document.createElement('div');
+      // TODO - log the route color + difficulty too bc it'll change
+      d.innerText = `[Color] [diff] (${c.routenum}): ${c.pct}%`;
+      if (c.notes) {
+        d.innerText += ` - ${c.notes}`;
+      }
+      dialogDiv.appendChild(d);
+    }
+  }
+}
+
+async function fetchAndUpdateRouteInfo() {
+  const routeInfo = await fetchEndpoint('fetchroutes');
+  for (const info of routeInfo) {
+    const routeDiv = document.querySelector(`#id${info.routenum}`);
+    if (!routeDiv) { continue; }
+    if (info.color) { routeDiv.classList.add(info.color); }
+    if (info.difficulty) { routeDiv.setAttribute('difficulty', info.difficulty); }
+  }
+}
+
+function submitLog() {
+  // TODO - log the route color + difficulty too bc it'll change
+  logClimb(numSpan.innerText, dateInput.value, pctInput.value, companionsInput.value, notesInput.value);
+  closeLog();
+}
+
+function updateRoute(routenum, color, difficulty) {
+  if (!routenum || !color || !difficulty) {
+    // console.log(`routenum (${routenum}), color (${color}), and difficulty (${difficulty}) must all be defined to update a route`);
+    return;
+  }
+
+  fetchEndpoint('update', `routenum=${routenum}&color=${color}&difficulty=${difficulty}`);
+}
+
+function logClimb(routenum, date, pct, companions, notes) {
+  if (!routenum || !date || !pct) {
+    // console.log(`routenum (${routenum}), date (${date}), and pct (${pct}) must all be defined to log a climb`);
+    return;
+  }
+  fetchEndpoint('logclimb', `routenum=${routenum}&date=${date}&pct=${pct}&companions=${companions}&notes=${notes}`);
+}
+
+async function fetchEndpoint(endpoint, body) {
+  // console.log(`fetching ${endpoint}`);
+  const response = await fetch(`/climbing/${endpoint}.php`, {
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    method: "POST",
+    body: body,
+  });
+  const responseBody = await response.text();
+  // console.log(responseBody);
+  return JSON.parse(responseBody);
 }
 
 function getColorFromClasslist(div) {
--- update.php
+++ update.php
@@ -2,37 +2,36 @@
 
 /*
 *  Usage: POST update.php
-*    routenum: "Apples" // Number of route to update
+*    routenum: 46 // Number of route to update
 *    color: "yellow" // Color of route to set
-*    difficulty: "7" // 5.X difficulty score to set
+*    difficulty: 7 // 5.X difficulty score to set
 *
 *  Returns: JSON representation of new route, or object with "Error" key if failure
 */
 
   include("common.php");
-
-  $params = json_decode(file_get_contents('php://input'));
-
-  $name = mysqli_real_escape_string($db, $params->name);
-  if (empty($name)) die('{"Error":"Name cannot be empty"}');
-  $value = mysqli_real_escape_string($db, $params->value);
-  if (empty($value)) {
-    $value = 0;
+  if (isset($_POST["routenum"])) {
+    $routenum = $_POST["routenum"];
   }
-  $date = mysqli_real_escape_string($db, $params->date);
-  if (empty($date)) die('{"Error":"Date cannot be empty"}');
-
-  $datestr = date ("Y-m-d", strtotime($date));
-
-  $sql = "INSERT INTO logs (name, value, date) VALUES ('$name', $value, '$datestr');";
+  if (empty($routenum)) die('{"Error":"routenum cannot be empty"}');
+  if (isset($_POST["color"])) {
+    $color = mysqli_real_escape_string($db, $_POST["color"]);
+  }
+  if (empty($color)) die('{"Error":"color cannot be empty"}');
+  if (isset($_POST["difficulty"])) {
+    $difficulty = $_POST["difficulty"];
+  }
+  if (empty($difficulty)) die('{"Error":"difficulty cannot be empty"}');
+  
+  $sql = "UPDATE routes SET `color`='$color',`difficulty`='$difficulty' WHERE `routenum`='$routenum';";
  
-  echo("<br><br>SQL: $sql<br>");
+  echo("SQL: $sql ");
   
   $result = mysqli_query($db, $sql) or die('{"Error":"'+mysqli_error($db)+'"}');
 
-  echo("Executed<br>");
+  echo("Executed. ");
   
-  $result = mysqli_query($db, "SELECT * FROM logs WHERE id = {$db->insert_id} LIMIT 1") or die('{"Error":"'+mysqli_error($db)+'"}');
+  $result = mysqli_query($db, "SELECT * FROM routes WHERE routenum = $routenum LIMIT 1") or die('{"Error":"'+mysqli_error($db)+'"}');
   $entry = mysqli_fetch_assoc($result);
 
   echo(json_encode($entry, JSON_NUMERIC_CHECK));
